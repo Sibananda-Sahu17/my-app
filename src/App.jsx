fe1a24f75216fabfb182577b4020981f0da9030a
Sibananda Sahu <sibananda6481@gmail.com>
2023-05-26 10:58:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <div className='font-bold text-center p-20'>Hello is working!!!!</div>
+    </div>
+  )
+}
+
+export default App
